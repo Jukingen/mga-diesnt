d1b44389cdc23b0eaa6689a9efe1abdbd8adcbba
MB <a.terziev@live.at>
2026-02-14 22:11:32 +0100

    Set up new Astro website with Tailwind CSS, initial styling, and core page structure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  output: 'static',
+  site: 'https://miga-dienstleistungen.at',
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
+});
